8f942807e585f539b06ebbe3fa1aeb9da9c4e76c
gaurab319 <gaurab625@gmail.com>
2022-06-23 14:59:04 +0545

    Add project files.

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(MVCLogin.User userModel)
+        {
+            using(LoginDataBaseEntities db=new LoginDataBaseEntities())
+            {
+                var userDetails=db.Users.Where(x=>x.UserName==userModel.UserName && x.Password==userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong UserName or Password";
+                    return View("Index", userModel);
+                }
+                else
+                    Session["userId"] = userDetails.UserId;
+                Session["username"] = userDetails.UserName;
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
